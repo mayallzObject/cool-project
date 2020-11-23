3105879ccdf8303a97e99154812bd993e65aea0a
mayallzObject <mayallzobject@gmail.com>
2020-11-23 01:22:36 +0100

    search-bar setup

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    SearchBar
+  <div class="search-bar">
+    <img class="weather-icon" src="../assets/a01d.png" />
+    <select class="select-box" id="countries">
+      <option value="hi">hi</option>
+      <option value="bey">bey</option>
+    </select>
+    <div class="search-box">
+      <input class="search-city" />
+      <button class="search">S</button>
+    </div>
   </div>
 </template>
 
@@ -10,4 +18,70 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.search-bar {
+  position: absolute;
+  width: 632px;
+  height: 92px;
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.9),
+      rgba(255, 255, 255, 0.9)
+    ),
+    #f8f8f8;
+  box-shadow: 0px 2px 10px rgba(8, 21, 62, 0.15);
+  border-radius: 16px;
+}
+.select-box {
+  position: absolute;
+  max-width: 95px;
+  height: 48px;
+  width: 100%;
+  top: 22px;
+  left: 82px;
+  border: 1px solid rgba(8, 21, 62, 0.05);
+  border-radius: 6px;
+  background: #ffffff;
+}
+.weather-icon {
+  position: absolute;
+  height: 48px;
+  width: 48px;
+  top: 22px;
+  left: 22px;
+}
+.search-box {
+  position: absolute;
+  max-width: 425px;
+  width: 100%;
+  height: 48px;
+  top: 22px;
+  left: 185px;
+  background: #ffffff;
+  border: 1px solid rgba(8, 21, 62, 0.05);
+  border-radius: 6px;
+}
+.search-city {
+  position: absolute;
+  max-width: 299px;
+  width: 100%;
+  height: 30px;
+  left: 17px;
+  top: 9px;
+  border: none;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 21px;
+  color: #08153e;
+  opacity: 0.5;
+}
+.search {
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 12px;
+  left: 382px;
+}
+</style>
