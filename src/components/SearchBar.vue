6c794d37731816202dd9dea54882252d6f9b2f40
mayallzObject <mayallzobject@gmail.com>
2020-12-21 06:10:20 +0100

    rwd fix

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -122,7 +122,7 @@ export default {
   /* Positions background arrow image */
   background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAh0lEQVQ4T93TMQrCUAzG8V9x8QziiYSuXdzFC7h4AcELOPQAdXYovZCHEATlgQV5GFTe1ozJlz/kS1IpjKqw3wQBVyy++JI0y1GTe7DCBbMAckeNIQKk/BanALBB+16LtnDELoMcsM/BESDlz2heDR3WePwKSLo5eoxz3z6NNcFD+vu3ij14Aqz/DxGbKB7CAAAAAElFTkSuQmCC');
   background-repeat: no-repeat;
-  background-position: 2rem center;
+  background-position: 3rem center;
 }
 
 .select-box:hover {
